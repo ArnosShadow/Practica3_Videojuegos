fd58ba9388ac4fb6ecbd5df43ba38c8309145877
Marta-678 <martaroldan972@gmail.com>
2025-04-05 20:19:12 +0200

    terminar trampa baldosa

--- Assets/Marta/Script/FallingTile.cs
+++ Assets/Marta/Script/FallingTile.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class FallingTile : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true;
+            rb.useGravity = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (rb != null)
+            {
+                rb.isKinematic = false;
+                rb.useGravity = true;
+            }
+        }
     }
 }
