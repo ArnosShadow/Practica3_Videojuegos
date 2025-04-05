254cde825c7653a57188b29fcd9fd91a5aee56e4
Marta-678 <martaroldan972@gmail.com>
2025-04-05 16:26:37 +0200

    Diseñar la trampa de baldosas

---
+++ Assets/Marta/Script/FallingTrap.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FallingTrap : MonoBehaviour
+{
+    
+    void Start()
+    {
+        //A través del padre voy a acceder a los hijos que tengas cierta layer. 
+
+        //por cada ijo 
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
