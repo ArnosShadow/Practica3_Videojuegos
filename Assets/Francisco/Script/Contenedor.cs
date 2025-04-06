d98033d496ee839d3ceebf38d8be6e50e5850aff
Arnos <fj.jaraba.estevez@gmail.com>
2025-04-06 21:33:36 +0200

    Se ha incluido personalización del tamaño del mapa

---
+++ Assets/Francisco/Script/Contenedor.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Contenedor", menuName = "Francisco/Script/Contenedor")]
+public class Contenedor : ScriptableObject
+{
+    public int ancho;
+    public int alto;
+}
